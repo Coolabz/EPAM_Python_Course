70d1170b68bd58feb7c5ef575b9edf1a60b022e8
Nick <105433239+Coolabz@users.noreply.github.com>
2022-09-25 15:59:39 +0300

    Update setup.py

--- final_task/setup.py
+++ final_task/setup.py
@@ -7,7 +7,7 @@ with open("README.md", "r") as file:
 setup(
     name='rss-reader',
     version='4.0',
-    packages=find_packages(),
+    packages=['Rss_Parser'],
     description='Python RSS reader',
     long_description=description,
     long_description_content_type='text/markdown',
@@ -15,5 +15,10 @@ setup(
     author='Mikalai Shupayeu',
     author_email='Mikalai_Shupayeu@epam.com',
     install_requires=['aspose-words==22.9.0', 'certifi==2022.9.24', 'charset-normalizer==2.1.1', 'feedparser==6.0.10', 'fpdf==1.7.2', 'idna==3.4','requests==2.28.1', 'sgmllib3k==1.0.0', 'sgmllib3k==1.0.0', 'urllib3==1.26.12'],
-    entry_points={'console_scripts': ['rss-reader = rss_parser.main:main']}
+    entry_points={
+        'console_scripts': [
+            'rss_parser = Rss_Parser.main:main',
+        ],
+    },
+    test_suite='RSS_Parser.test'
 )
