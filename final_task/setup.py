bf8f211fda4dfa7e6380dc59005ab72ab743679c
Nick <105433239+Coolabz@users.noreply.github.com>
2022-09-25 23:12:32 +0300

    Update setup.py

--- final_task/setup.py
+++ final_task/setup.py
@@ -14,6 +14,14 @@ setup(
     url='https://github.com/Coolabz/EPAM_Python_Course',
     author='Mikalai Shupayeu',
     author_email='Mikalai_Shupayeu@epam.com',
+    scripts=['RSS_Parser/argparser_rss.py',
+             'RSS_Parser/Cache_helper.py',
+             'RSS_Parser/DateFunc.py',
+             'RSS_Parser/Exceptions.py',
+             'RSS_Parser/main.py',
+             'RSS_Parser/rss_parser_class.py',
+             'RSS_Parser/to_epub.py',
+             'RSS_Parser/to_pdf.py'],
     install_requires=['aspose-words==22.9.0', 'certifi==2022.9.24', 'charset-normalizer==2.1.1', 'feedparser==6.0.10', 'fpdf==1.7.2', 'idna==3.4','requests==2.28.1', 'sgmllib3k==1.0.0', 'sgmllib3k==1.0.0', 'urllib3==1.26.12'],
     entry_points={
         'console_scripts': [
